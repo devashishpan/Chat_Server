7a452999d1e5822ce34109700ca369037aea068a
D09 <>
2022-01-30 22:17:33 +0530

    Created a login Screen.

--- ChatClientGUI.py
+++ ChatClientGUI.py
@@ -1,17 +1,29 @@
 import sys
 
+from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QDialog, QWidget, QStackedWidget
+
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login, self).__init__()
+        loadUi("login.ui", self)
 
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(100, 100, 500, 500)
-    win.setWindowTitle("PING")
-    win.setWindowIcon(QtGui.QIcon("ping.jpeg"))
-    win.show()
-    sys.exit(app.exec_())
+    login = Login()
+    widget = QStackedWidget()
+    widget.addWidget(login)
+    widget.setFixedHeight(300)
+    widget.setFixedWidth(450)
+    widget.show()
+
+    try:
+        sys.exit(app.exec())
+    except:
+        print("Exiting....")
 
 
 if __name__ == '__main__':
