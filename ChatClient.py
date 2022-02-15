a67fb9aaa8181607ded29c869b29fbe9032713b3
D09 <57136627+devashishpan@users.noreply.github.com>
2022-02-15 18:03:10 +0530

    chat gui

--- ChatClient.py
+++ ChatClient.py
@@ -1,11 +1,12 @@
+import os.path
 import re
 import socket
 import sys
 from threading import *
 
 from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QDialog, QStackedWidget
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QApplication, QDialog, QStackedWidget, QFileDialog, QMessageBox, QLabel
 
 
 class Client:
@@ -32,8 +33,7 @@ class Client:
                 print(e)
                 return False
 
-    def send_message(self):
-        message = input('SEND>> ')
+    def send_message(self, message):
         self.new_socket.send(f'{self.host_ip}${message}'.encode())
 
     def recv_message(self):
@@ -45,7 +45,7 @@ class Client:
                 self.new_socket.close()
                 sys.exit(0)
             sender, message = tuple(recv_msg.decode().split('$'))
-            print(f'\n[{sender}]>> {message}')
+            return f'\n[{sender}]>> {message}'
         self.new_socket.close()
 
     def converse(self, server_ip):
@@ -53,20 +53,10 @@ class Client:
         self.connect_()
 
 
+BUFFER_SIZE = 4 * 1024
 regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 s_ip = ""
 start_client = Client()
-
-
-class Send(Thread):
-    def run(self):
-        while start_client.check_connection():
-            start_client.send_message()
-
-
-class Recv(Thread):
-    def run(self):
-        start_client.recv_message()
 
 
 class WelcomeScreen(QDialog):
@@ -81,22 +71,103 @@ class WelcomeScreen(QDialog):
             s_ip = self.server_ip.text()
             try:
                 start_client.converse(s_ip)
-                self.close()
+                widget.addWidget(main_window)
+                widget.setFixedHeight(450)
+                widget.setFixedWidth(600)
+                widget.setCurrentIndex(widget.currentIndex() + 1)
             except Exception as e:
                 self.error.setText(f"{e}Please Enter a valid IP....")
         else:
             self.error.setText("Please Enter a valid IP....")
 
 
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("main_window.ui", self)
+        self.file.clicked.connect(self.send_file)
+        self.progressBar.setVisible(False)
+
+    def send_file(self):
+        path = QFileDialog.getOpenFileNames(self, 'Open a File', '', 'All Files (*.*)')
+        filename = path[0][0]
+        filesize = os.path.getsize(filename)
+        total = round(filesize / BUFFER_SIZE) + 1
+        completed = 0
+        if path == ([], ''):
+            self.progressBar.setVisible(True)
+            start_client.new_socket.send(f"{filename}${filesize}".encode())
+            self.send_files(f"{filename}")
+            with open(filename, "rb") as f:
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    print("File is empty!!")
+                start_client.new_socket.sendall(bytes_read)
+                completed += total / 100
+                self.progressBar.setValue(completed)
+        else:
+            return 0
+        self.send_completed()
+        self.progressBar.setVisible(False)
+
+    @staticmethod
+    def send_completed(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("File Send Complete!!!!")
+        msg.setWindowTitle("Information MessageBox")
+        msg.setStandardButtons(QMessageBox.Ok)
+        print(msg.exec_())
+
+    def send_messages(self, message):
+        label = QLabel(message)
+        label.setAlignment(QtCore.Qt.AlignRight)
+        self.messages.addWidget(label)
+
+    def received_message(self, message):
+        label = QLabel(message)
+        label.setAlignment(QtCore.Qt.AlignLeft)
+        self.messages.addWidget(label)
+
+    def send_files(self, message):
+        label = QLabel(message)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+        label.adjustSize()
+        label.width(140)
+        self.messages.addWidget(label)
+
+
 app = QApplication(sys.argv)
 app.setWindowIcon(QtGui.QIcon("ping.jpeg"))
-welcome = ServerLogin()
+welcome = MainWindow()
+main_window = MainWindow()
 widget = QStackedWidget()
 widget.setWindowTitle("PING")
 widget.addWidget(welcome)
 widget.setFixedHeight(300)
 widget.setFixedWidth(450)
 widget.show()
+
+try:
+    sys.exit(app.exec())
+except:
+    print("Exiting....")
+
+
+class Send(Thread):
+    def run(self):
+        while start_client.check_connection():
+            message = main_window.message_box.text()
+            main_window.send_messages(message)
+            start_client.send_message(message)
+
+
+class Recv(Thread):
+    def run(self):
+        message = start_client.recv_message()
+        main_window.received_message(message)
+
+
 if __name__ == '__main__':
     pass
     # Recv().start()
